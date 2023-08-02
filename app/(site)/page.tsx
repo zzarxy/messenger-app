f3544d8caea5e5654d78bd8a3822dd3d8a449245
zarxy <zarxymen@gmail.com>
2023-08-02 15:33:01 +0300

    feat: add title text

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -2,8 +2,13 @@ import AuthForm from "./components/AuthForm";
 
 export default function Home() {
   return (
-    <div>
-      <AuthForm />
+    <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8 bg-zinc-800">
+      <div>
+        <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-100">
+          Sign in to your account
+        </h2>
+        <AuthForm />
+      </div>
     </div>
   );
 }
