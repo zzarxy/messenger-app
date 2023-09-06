ec9612ee28b0c3f43ef79b4d818b1d38278ba0d6
zarxy <zarxymen@gmail.com>
2023-09-06 12:02:10 +0300

    fix: incorrect conversation information

--- app/actions/getConversations.ts
+++ app/actions/getConversations.ts
@@ -2,37 +2,44 @@ import prisma from "@/app/libs/prismadb";
 import getCurrentUser from "./getCurrentUser";
 
 const getConversations = async () => {
-    const currentUser = await getCurrentUser();
+   const currentUser = await getCurrentUser();
 
-    if (!currentUser?.id) {
-        return [];
-    }
+   if (!currentUser?.id) {
+      return [];
+   }
 
-    try {
-        const conversations = await prisma.conversation.findMany({
-            orderBy: {
-                lastMessageAt: 'desc'
-            },
-            where: {
-                userIds: {
-                    has: currentUser.id
-                }
-            },
-            include: {
-                users: true,
-                messages: {
-                    include: {
-                        sender: true,
-                        seen: true,
-                    }
-                },
+   try {
+      const conversations = await prisma.conversation.findMany({
+         orderBy: {
+            lastMessageAt: 'desc'
+         },
+         where: {
+            userIds: {
+               has: currentUser.id
             }
-        });
+         },
+         include: {
+            // return only users which id not equal currentUser.id
+            users: {
+               where: {
+                  NOT: {
+                     id: currentUser.id
+                  }
+               }
+            },
+            messages: {
+               include: {
+                  sender: true,
+                  seen: true,
+               }
+            },
+         }
+      });
 
-        return conversations;
-    } catch (error: any) {
-        return [];
-    }
+      return conversations;
+   } catch (error: any) {
+      return [];
+   }
 }
 
 export default getConversations;
