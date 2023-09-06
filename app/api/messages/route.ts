8fc8772ea492b5ff6636adf9cfba30b3fd0f20ef
zarxy <zarxymen@gmail.com>
2023-09-06 11:38:48 +0300

    fix: pusher fix of updating last masage

--- app/api/messages/route.ts
+++ app/api/messages/route.ts
@@ -4,99 +4,99 @@ import prisma from "@/app/libs/prismadb"
 import { pusherServer } from "@/app/libs/pusher"
 
 export async function POST(
-    request: Request
+   request: Request
 ) {
-    try {
-        const currentUser = await getCurrentUser()
-        const body = await request.json()
-        const {
-            message,
-            image,
-            conversationId
-        } = body;
+   try {
+      const currentUser = await getCurrentUser()
+      const body = await request.json()
+      const {
+         message,
+         image,
+         conversationId
+      } = body;
 
-        if (!currentUser?.id || !currentUser?.email) {
-            return new NextResponse('Unauthorized', { status: 401 })
-        }
+      if (!currentUser?.id || !currentUser?.email) {
+         return new NextResponse('Unauthorized', { status: 401 })
+      }
 
-        const newMessage = await prisma.message.create({
-            data: {
-                body: message,
-                image: image,
-                conversation: {
-                    connect: {
-                        id: conversationId
-                    }
-                },
-                sender: {
-                    connect: {
-                        id: currentUser.id
-                    }
-                },
-                seen: {
-                    connect: {
-                        id: currentUser.id
-                    }
-                }
+      const newMessage = await prisma.message.create({
+         data: {
+            body: message,
+            image: image,
+            conversation: {
+               connect: {
+                  id: conversationId
+               }
             },
-            // Return seen and sender objects how response on post request of message
-            //
-            include: {
-                seen: {
-                    select: {
-                        id: true,
-                        name: true,
-                        email: true,
-                        image: true
-                    }
-                },
-                sender: {
-                    select: {
-                        id: true,
-                        name: true,
-                        email: true,
-                        image: true
-                    }
-                }
+            sender: {
+               connect: {
+                  id: currentUser.id
+               }
+            },
+            seen: {
+               connect: {
+                  id: currentUser.id
+               }
             }
-        })
-
-        const updatedConversation = await prisma.conversation.update({
-            where: {
-                id: conversationId
+         },
+         // Return seen and sender objects how response on post request of message
+         //
+         include: {
+            seen: {
+               select: {
+                  id: true,
+                  name: true,
+                  email: true,
+                  image: true
+               }
             },
-            data: {
-                lastMessageAt: new Date(),
-                messages: {
-                    connect: {
-                        id: newMessage.id
-                    }
-                }
-            },
-            include: {
-                users: true,
-                messages: {
-                    include: {
-                        seen: true
-                    }
-                }
+            sender: {
+               select: {
+                  id: true,
+                  name: true,
+                  email: true,
+                  image: true
+               }
             }
-        })
+         }
+      })
 
-        await pusherServer.trigger(conversationId, 'messages:new', newMessage);
+      const updatedConversation = await prisma.conversation.update({
+         where: {
+            id: conversationId
+         },
+         data: {
+            lastMessageAt: new Date(),
+            messages: {
+               connect: {
+                  id: newMessage.id
+               }
+            }
+         },
+         include: {
+            users: true,
+            messages: {
+               include: {
+                  seen: true
+               }
+            }
+         }
+      })
 
-        const lastMessage = updatedConversation.messages[updatedConversation.messages.length - 1];
+      await pusherServer.trigger(conversationId, 'messages:new', newMessage);
 
-        updatedConversation.users.map((user) => {
-            pusherServer.trigger(user.email!, 'conversation:update', {
-                id: conversationId,
-                message: [lastMessage]
-            })
-        });
+      const lastMessage = updatedConversation.messages[updatedConversation.messages.length - 1];
 
-        return NextResponse.json(newMessage)
-    } catch (error: any) {
-        console.log(error, 'ERROR_MESSAGES')
-        return new NextResponse('IternalError', { status: 500 })
-    };
+      updatedConversation.users.map((user) => {
+         pusherServer.trigger(user.email!, 'conversation:update', {
+            id: conversationId,
+            messages: [lastMessage]
+         })
+      });
+
+      return NextResponse.json(newMessage)
+   } catch (error: any) {
+      console.log(error, 'ERROR_MESSAGES')
+      return new NextResponse('IternalError', { status: 500 })
+   };
 }
